10f5d95ac634e5af4ca60baf4d3b17e0bf1bac4b
Ivan Marcos <imarcos@aldaba.es>
2025-02-19 17:27:53 +0100

    estilos y estado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import BookCreate from "./components/BookCreate";
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Starter Project</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.js</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    const [books, setBooks] = useState([]);
+
+    //const handleCreateBook
+    const createBoot = (title) => {
+        //console.log('Need to add book with:',title);
+    };
+
+    return (
+        <div>
+            <BookCreate onSubmit={createBoot}/>
+        </div>
+    );
 }
 
 export default App;
